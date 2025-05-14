6107983d5017db5126712562a8fa896f9f92a793
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2025-05-14 23:24:51 +0700

    view fragment

---
+++ app/src/main/java/com/example/take_note/WebViewFragment.java
@@ -0,0 +1,41 @@
+package com.example.take_note;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class WebViewFragment extends Fragment {
+
+    private WebView webView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_webview, container, false);
+
+        webView = view.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient()); // Load trong app thay vì mở trình duyệt ngoài
+        webView.getSettings().setJavaScriptEnabled(true); // Cho phép JS nếu web yêu cầu
+
+        // Load trang web mong muốn
+        webView.loadUrl("https://www.google.com");
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (webView != null) {
+            webView.destroy();
+        }
+    }
+}
