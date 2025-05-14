a33b1bb6dbc3b33b3f65663d28b0306f540b406a
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2025-05-15 01:32:26 +0700

    add

--- app/src/main/java/com/example/take_note/NoteDatabaseHelper.java
+++ app/src/main/java/com/example/take_note/NoteDatabaseHelper.java
@@ -6,32 +6,36 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class NoteDatabaseHelper extends SQLiteOpenHelper {
 
-    // Tên cơ sở dữ liệu và phiên bản
-    private static final String DATABASE_NAME = "diary.db";
+    private static final String DATABASE_NAME = "notes.db";
     private static final int DATABASE_VERSION = 1;
 
-    // Tên bảng và các cột
     public static final String TABLE_NAME = "notes";
-    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_ID = "_id";
     public static final String COLUMN_TITLE = "title";
     public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_CREATED_AT = "created_at";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_TITLE + " TEXT NOT NULL, " +
+                    COLUMN_CONTENT + " TEXT NOT NULL, " +
+                    COLUMN_CREATED_AT + " INTEGER);";
 
     public NoteDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    // Called when DB is created for the first time
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Câu lệnh tạo bảng
-        String createTable = "CREATE TABLE " + TABLE_NAME + " ("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_TITLE + " TEXT, "
-                + COLUMN_CONTENT + " TEXT);";
-        db.execSQL(createTable);
+        db.execSQL(TABLE_CREATE);
     }
 
+    // Called when DB version is incremented
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if it exists and recreate
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
